ddaa66c88c96002f330c5d7f42c8f40543851faa
André Sortland <arsortland@gmail.com>
2022-01-27 21:48:36 +0100

    cleaned up some global code and refactored some code

--- script.js
+++ script.js
@@ -15,26 +15,24 @@ const gameBoard =(() => {
         [midleft.textContent,midmid.textContent,midright.textContent],
         [botleft.textContent,botmid.textContent,botright.textContent]
     ];
-
     return {
         board,
     };
 })();
 
 
-const players = (marker) => {
-    const getMarker = () => marker;     //som en getmetode i Java!
+const player = (marker) => {
+    const getMarker = () => marker;
     return {
         getMarker,
     };
 };
 
 
-
-const displayController =(() => {
+const gameLogic =(() => {
     let i=0;
-    const playerX = players('X');
-    const playerO = players('O');
+    const playerX = player('X');
+    const playerO = player('O');
 
     const alterneTurns = (num) => {
         let alternate = (num%2==0) ? playerX.getMarker() : playerO.getMarker();
@@ -43,47 +41,59 @@ const displayController =(() => {
 
     const dontAllowOccupiedSpace = (space) => {
         if (space.textContent != ''){
-            i--;
             return space.textContent;
         }
-           return alterneTurns(i);
+           return alterneTurns(i++);
     }
 
-    const checkForWinOrDraw = () => {       //Run new function when draw/win;
+    const checkForWinOrDraw = () => {
         let brd = gameBoard.board();
         if ((brd[0][0] === 'X') && (brd[1][1] === 'X') && (brd[2][2] ==='X')){
-            console.log('diagonal win');
+            console.log('PlayerX wins');
         }else if ((brd[0][0] === 'O') && (brd[1][1] === 'O') && (brd[2][2] ==='O')){
-            console.log('diagonal win');
+            console.log('PlayerO wins');
         }else if ((brd[0][2] === 'X') && (brd[1][1] === 'X') && (brd[2][0] ==='X')){
-            console.log('diagonal win');
+            console.log('PlayerX wins');
         }else if ((brd[0][2] === 'O') && (brd[1][1] === 'O') && (brd[2][0] ==='O')){
-            console.log('diagonal win');
+            console.log('PlayerO wins');
         }
         for (let i=0; i<3;i++){
             if ((brd[i][0] ==='X') && (brd[i][1] === 'X') && (brd[i][2] === 'X')){
-                console.log('We have a winner! PLAYERX');
+                console.log('PlayerX wins');
             } else if ((brd[i][0] ==='O') && (brd[i][1] === 'O') && (brd[i][2] === 'O')){
-                console.log('We have a winner! PLAYERO');
+                console.log('PlayerO wins');
             }else if ((brd[0][i] ==='X') && (brd[1][i] === 'X') && (brd[2][i] === 'X')){
-                console.log('vertcal win')
+                console.log('PlayerX wins')
             }else if ((brd[0][i] ==='O') && (brd[1][i] === 'O') && (brd[2][i] === 'O')){
-                console.log('vertcal win')
+                console.log('PlayerO wins')
+            }else if (!brd[0].includes('') && !brd[1].includes('') && !brd[2].includes('')){
+                console.log('Draw');
             }
         }  
     }
 
-    const takeTurn = 
-        document.querySelectorAll('.elem').forEach(cell => cell.addEventListener('click', ()=> {
-        cell.textContent = dontAllowOccupiedSpace(cell);
-        i++;
-        checkForWinOrDraw();
-        // console.log(i);
-        // console.log(alterneTurns(i));
-        // console.log(gameBoard.board());
-    }))
+    const haveWon = () => {
+        //function for winning. Make html here!
+        //Modal?
+    }
+
+    const itsDraw = () => {
+        //function for draw! Make html here!
+        //Modal? Combine the two functions?
+    }
+
+    return {
+        dontAllowOccupiedSpace,
+        checkForWinOrDraw,
+        haveWon,
+    }
 })();
 
 
-
-//make a playGame module and clean up displaycontroller?
+const playGame = (()=> {
+    document.querySelectorAll('.elem').forEach(cell => cell.addEventListener('click', ()=> {
+        cell.textContent = gameLogic.dontAllowOccupiedSpace(cell);
+        gameLogic.checkForWinOrDraw();
+        gameLogic.haveWon();
+    }));
+})();
