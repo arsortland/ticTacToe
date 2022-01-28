9e85928c6d499298297f23d3388c1917ad71486d
André Sortland <arsortland@gmail.com>
2022-01-28 15:24:33 +0100

    player can now chose marker via button choice. Also changed if/else chain to switch for more readability

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ const gameBoard =(() => {
         [midleft.textContent,midmid.textContent,midright.textContent],
         [botleft.textContent,botmid.textContent,botright.textContent]
     ];
+
+    const clearBoard = (() =>{
+        document.querySelector('.restart').addEventListener('click', ()=> {
+            location.reload();
+        })
+    })();
+
     return {
         board,
     };
@@ -23,19 +30,37 @@ const gameBoard =(() => {
 
 const player = (marker) => {
     const getMarker = () => marker;
+    const setMark = (newMark) => marker = newMark; 
     return {
         getMarker,
+        setMark,
     };
 };
 
 
 const gameLogic =(() => {
-    let i=0;
-    const playerX = player('X');
-    const playerO = player('O');
+
+    const player1 = player('');
+    const player2 = player('');
+    const circlebtn = document.querySelector('.circle');
+    const crossbtn = document.querySelector('.cross');
+    circlebtn.addEventListener('click', () => {
+        player1.setMark('O');
+        player2.setMark('X');
+        circlebtn.disabled = true;
+        crossbtn.disabled = true;
+        return i=0;
+    });
+    crossbtn.addEventListener('click', () => {
+        player1.setMark('X');
+        player2.setMark('O');
+        crossbtn.disabled = true;
+        circlebtn.disabled = true;
+        return i=0;
+    });
 
     const alterneTurns = (num) => {
-        let alternate = (num%2==0) ? playerX.getMarker() : playerO.getMarker();
+        let alternate = (num%2==0) ? player1.getMarker() : player2.getMarker();
         return alternate;
     }
 
@@ -43,49 +68,80 @@ const gameLogic =(() => {
         if (space.textContent != ''){
             return space.textContent;
         }
-           return alterneTurns(i++);
+        try{
+            return alterneTurns(i++);
+        }
+        catch{
+            return;
+        }
+            
     }
 
     const checkForWinOrDraw = () => {
-        let brd = gameBoard.board();
-        if ((brd[0][0] === 'X') && (brd[1][1] === 'X') && (brd[2][2] ==='X')){
-            console.log('PlayerX wins');
-        }else if ((brd[0][0] === 'O') && (brd[1][1] === 'O') && (brd[2][2] ==='O')){
-            console.log('PlayerO wins');
-        }else if ((brd[0][2] === 'X') && (brd[1][1] === 'X') && (brd[2][0] ==='X')){
-            console.log('PlayerX wins');
-        }else if ((brd[0][2] === 'O') && (brd[1][1] === 'O') && (brd[2][0] ==='O')){
-            console.log('PlayerO wins');
+        try{
+            if (i >= 4){
+                let brd = gameBoard.board();
+                if ((brd[0][0] === 'X') && (brd[1][1] === 'X') && (brd[2][2] ==='X')){
+                    return haveWon('X');
+                }else if ((brd[0][0] === 'O') && (brd[1][1] === 'O') && (brd[2][2] ==='O')){
+                    return haveWon('O');
+                }else if ((brd[0][2] === 'X') && (brd[1][1] === 'X') && (brd[2][0] ==='X')){
+                return haveWon('X');
+                }else if ((brd[0][2] === 'O') && (brd[1][1] === 'O') && (brd[2][0] ==='O')){
+                    return haveWon('O');
+                }
+                for (let i=0; i<3;i++){
+                    if ((brd[i][0] ==='X') && (brd[i][1] === 'X') && (brd[i][2] === 'X')){
+                        return haveWon('X');
+                    } else if ((brd[i][0] ==='O') && (brd[i][1] === 'O') && (brd[i][2] === 'O')){
+                        return haveWon('O');
+                    }else if ((brd[0][i] ==='X') && (brd[1][i] === 'X') && (brd[2][i] === 'X')){
+                        return haveWon('X');
+                    }else if ((brd[0][i] ==='O') && (brd[1][i] === 'O') && (brd[2][i] === 'O')){
+                        return haveWon('O');
+                    }else if (!brd[0].includes('') && !brd[1].includes('') && !brd[2].includes('')){
+                        return haveWon('draw');
+                    }
+                }  
+            }        
         }
-        for (let i=0; i<3;i++){
-            if ((brd[i][0] ==='X') && (brd[i][1] === 'X') && (brd[i][2] === 'X')){
-                console.log('PlayerX wins');
-            } else if ((brd[i][0] ==='O') && (brd[i][1] === 'O') && (brd[i][2] === 'O')){
-                console.log('PlayerO wins');
-            }else if ((brd[0][i] ==='X') && (brd[1][i] === 'X') && (brd[2][i] === 'X')){
-                console.log('PlayerX wins')
-            }else if ((brd[0][i] ==='O') && (brd[1][i] === 'O') && (brd[2][i] === 'O')){
-                console.log('PlayerO wins')
-            }else if (!brd[0].includes('') && !brd[1].includes('') && !brd[2].includes('')){
-                console.log('Draw');
+        catch{
+            return;
+        }
+
+    }
+
+    const haveWon = (res) => {
+        let modal = document.getElementById('myModal');
+        let modalContent = document.querySelector('.modal-content');
+        const gamewindow = document.querySelector('.gamewindow');
+        const blureffect = () => gamewindow.style.filter = 'blur(8px)';
+        window.onclick = (e) => {
+            if (e.target == modal){
+                location.reload();
             }
-        }  
+        }
+        switch (res){
+            case 'X':
+                modalContent.textContent = 'Player X have won!'
+                modal.style.display = 'block';
+                blureffect();
+                break;
+            case 'O':
+                modalContent.textContent = 'Player O have won!'
+                modal.style.display = 'block';
+                blureffect();
+                break;
+            case 'draw':
+                modalContent.textContent = "It's a draw!"
+                modal.style.display = 'block';
+                blureffect();
+                break;
+        }
     }
-
-    const haveWon = () => {
-        //function for winning. Make html here!
-        //Modal?
-    }
-
-    const itsDraw = () => {
-        //function for draw! Make html here!
-        //Modal? Combine the two functions?
-    }
-
     return {
         dontAllowOccupiedSpace,
         checkForWinOrDraw,
-        haveWon,
     }
 })();
 
@@ -94,6 +150,10 @@ const playGame = (()=> {
     document.querySelectorAll('.elem').forEach(cell => cell.addEventListener('click', ()=> {
         cell.textContent = gameLogic.dontAllowOccupiedSpace(cell);
         gameLogic.checkForWinOrDraw();
-        gameLogic.haveWon();
     }));
 })();
+
+
+//Figure out how to CPU. In player (how to use the playerfactory for it? Check TOP lecture) or gamelogic? Disable the other player? Make a new IIEFE for that and disable the other with a false/true?
+//Refactor the wincondition function????
+//
