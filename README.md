c5acd8eebcd1dbf7cea764d604b49883e6dfc1c7
Andre Sortland <arsortland@gmail.com>
2022-02-18 15:56:58 +0100

    testing git bash

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # ticTacToe
 Tic tac toe game!<br /><br />
 
+<u>What i learned:</u> <br>
+* Using IIFE and applying them in code.
+* Using playerfactories.
+* Understanding and using closures.
 
-What i am happy with:<br /><br />
 
--Got the code away from the global scope.<br />
--Understand the use of scope, closures and modules.<br />
--Letting each function do one thing.<br />
+<u>What i am happy with:</u><br />
 
-What would i do differently next time?:<br /><br />
+* Got the code away from the global scope.<br />
+* Understand the use of scope, closures and modules.<br />
+* Letting each function do one thing.<br />
 
--Plan it out with even more detail. In writing.<br />
--Make an 1D array and display win-conditions as a list with combinations.<br />
--Make the board in javascript.<br />
+<u>What would i do differently next time?:</u><br />
+
+* Plan it out with even more detail. In writing.<br />
+* Make an 1D array and display win-conditions as a list with combinations.<br />
+* Make the board in javascript.<br />
--- script.js
+++ script.js
@@ -1,162 +1,191 @@
-const gameBoard =(() => {
-    const topleft = document.getElementById('topleft');
-    const topmid = document.getElementById('topmid');
-    const topright = document.getElementById('topright');
-    const midleft = document.getElementById('midleft');
-    const midmid = document.getElementById('midmid');
-    const midright = document.getElementById('midright');
-    const botleft = document.getElementById('botleft');
-    const botmid = document.getElementById('botmid');
-    const botright = document.getElementById('botright');
-    const board = () =>
-    [
-        [topleft.textContent,topmid.textContent,topright.textContent],
-        [midleft.textContent,midmid.textContent,midright.textContent],
-        [botleft.textContent,botmid.textContent,botright.textContent]
-    ];
-    const clearBoard = (() =>{
-        document.querySelector('.restart').addEventListener('click', ()=> {
-            location.reload();
-        })
-    })();
-    return {
-        board,
-    };
+const gameBoard = (() => {
+  const topleft = document.getElementById("topleft");
+  const topmid = document.getElementById("topmid");
+  const topright = document.getElementById("topright");
+  const midleft = document.getElementById("midleft");
+  const midmid = document.getElementById("midmid");
+  const midright = document.getElementById("midright");
+  const botleft = document.getElementById("botleft");
+  const botmid = document.getElementById("botmid");
+  const botright = document.getElementById("botright");
+  const board = () => [
+    [topleft.textContent, topmid.textContent, topright.textContent],
+    [midleft.textContent, midmid.textContent, midright.textContent],
+    [botleft.textContent, botmid.textContent, botright.textContent],
+  ];
+  const clearBoard = (() => {
+    document.querySelector(".restart").addEventListener("click", () => {
+      location.reload();
+    });
+  })();
+  return {
+    board,
+  };
 })();
-
 
 const player = (marker) => {
-    const getMarker = () => marker;
-    const setMark = (newMark) => marker = newMark; 
-    return {
-        getMarker,
-        setMark,
-    };
+  const getMarker = () => marker;
+  const setMark = (newMark) => (marker = newMark);
+  return {
+    getMarker,
+    setMark,
+  };
 };
 
+const gameLogic = (() => {
+  let i = 0;
+  const player1 = player("X");
+  const player2 = player("O");
+  const circlebtn = document.querySelector(".circle");
+  const crossbtn = document.querySelector(".cross");
+  const whosNext = document.querySelector(".whosturn");
 
-const gameLogic =(() => {
-    let i=0;
-    const player1 = player('X');
-    const player2 = player('O');
-    const circlebtn = document.querySelector('.circle');
-    const crossbtn = document.querySelector('.cross');
-    const whosNext = document.querySelector('.whosturn');
-
-    circlebtn.addEventListener('click', () => {
-        document.querySelectorAll('.elem').forEach(cell => cell.textContent = '');
-        player1.setMark('O');
-        player2.setMark('X');
-        whosNext.textContent = 'Player O begins!';
-        return i=0;
-    });
-    crossbtn.addEventListener('click', () => {
-        document.querySelectorAll('.elem').forEach(cell => cell.textContent = '');
-        player1.setMark('X');
-        player2.setMark('O');
-        whosNext.textContent = 'Player X begins!';
-        return i=0;
-    });
-    const alterneTurns = (num) => {
-        let alternate = (num%2==0) ? player1.getMarker() : player2.getMarker();
-        return alternate;
+  circlebtn.addEventListener("click", () => {
+    document
+      .querySelectorAll(".elem")
+      .forEach((cell) => (cell.textContent = ""));
+    player1.setMark("O");
+    player2.setMark("X");
+    whosNext.textContent = "Player O begins!";
+    return (i = 0);
+  });
+  crossbtn.addEventListener("click", () => {
+    document
+      .querySelectorAll(".elem")
+      .forEach((cell) => (cell.textContent = ""));
+    player1.setMark("X");
+    player2.setMark("O");
+    whosNext.textContent = "Player X begins!";
+    return (i = 0);
+  });
+  const alterneTurns = (num) => {
+    let alternate = num % 2 == 0 ? player1.getMarker() : player2.getMarker();
+    return alternate;
+  };
+  const dontAllowOccupiedSpace = (space) => {
+    if (space.textContent != "") {
+      return space.textContent;
     }
-    const dontAllowOccupiedSpace = (space) => {
-        if (space.textContent != ''){
-            return space.textContent;
-        }
-        try{
-            return alterneTurns(i++);
-        }
-        catch{
-            return;
-        } 
+    try {
+      return alterneTurns(i++);
+    } catch {
+      return;
     }
-    const checkForWinOrDraw = () => {
-        try{
-            if (i >= 4){
-                let brd = gameBoard.board();
-                if ((brd[0][0] === 'X') && (brd[1][1] === 'X') && (brd[2][2] ==='X')){
-                    return haveWon('X');
-                }else if ((brd[0][0] === 'O') && (brd[1][1] === 'O') && (brd[2][2] ==='O')){
-                    return haveWon('O');
-                }else if ((brd[0][2] === 'X') && (brd[1][1] === 'X') && (brd[2][0] ==='X')){
-                return haveWon('X');
-                }else if ((brd[0][2] === 'O') && (brd[1][1] === 'O') && (brd[2][0] ==='O')){
-                    return haveWon('O');
-                }
-                for (let i=0; i<3;i++){
-                    if ((brd[i][0] ==='X') && (brd[i][1] === 'X') && (brd[i][2] === 'X')){
-                        return haveWon('X');
-                    } else if ((brd[i][0] ==='O') && (brd[i][1] === 'O') && (brd[i][2] === 'O')){
-                        return haveWon('O');
-                    }else if ((brd[0][i] ==='X') && (brd[1][i] === 'X') && (brd[2][i] === 'X')){
-                        return haveWon('X');
-                    }else if ((brd[0][i] ==='O') && (brd[1][i] === 'O') && (brd[2][i] === 'O')){
-                        return haveWon('O');
-                    }
-                }
-                for (let i=0; i<3;i++){ //moved here to check for all winpossibilites before looking for draw.
-                    if (!brd[0].includes('') && !brd[1].includes('') && !brd[2].includes('')){
-                        return haveWon('draw');
-                    }
-                }  
-            }        
+  };
+  const checkForWinOrDraw = () => {
+    try {
+      if (i >= 4) {
+        let brd = gameBoard.board();
+        if (brd[0][0] === "X" && brd[1][1] === "X" && brd[2][2] === "X") {
+          return haveWon("X");
+        } else if (
+          brd[0][0] === "O" &&
+          brd[1][1] === "O" &&
+          brd[2][2] === "O"
+        ) {
+          return haveWon("O");
+        } else if (
+          brd[0][2] === "X" &&
+          brd[1][1] === "X" &&
+          brd[2][0] === "X"
+        ) {
+          return haveWon("X");
+        } else if (
+          brd[0][2] === "O" &&
+          brd[1][1] === "O" &&
+          brd[2][0] === "O"
+        ) {
+          return haveWon("O");
         }
-        catch{
-            return;
+        for (let i = 0; i < 3; i++) {
+          if (brd[i][0] === "X" && brd[i][1] === "X" && brd[i][2] === "X") {
+            return haveWon("X");
+          } else if (
+            brd[i][0] === "O" &&
+            brd[i][1] === "O" &&
+            brd[i][2] === "O"
+          ) {
+            return haveWon("O");
+          } else if (
+            brd[0][i] === "X" &&
+            brd[1][i] === "X" &&
+            brd[2][i] === "X"
+          ) {
+            return haveWon("X");
+          } else if (
+            brd[0][i] === "O" &&
+            brd[1][i] === "O" &&
+            brd[2][i] === "O"
+          ) {
+            return haveWon("O");
+          }
         }
+        for (let i = 0; i < 3; i++) {
+          //moved here to check for all winpossibilites before looking for draw.
+          if (
+            !brd[0].includes("") &&
+            !brd[1].includes("") &&
+            !brd[2].includes("")
+          ) {
+            return haveWon("draw");
+          }
+        }
+      }
+    } catch {
+      return;
     }
-    //Displays the result on screen as a modal.
-    const haveWon = (res) => {
-        let modal = document.getElementById('myModal');
-        let modalContent = document.querySelector('.modal-content');
-        const gamewindow = document.querySelector('.gamewindow');
-        const blureffect = () => gamewindow.style.filter = 'blur(8px)';
-        window.onclick = (e) => {
-            if (e.target == modal){
-                location.reload();
-            }
-        }
-        switch (res){
-            case 'X':
-                modalContent.textContent = 'Player X have won!'
-                modal.style.display = 'block';
-                blureffect();
-                break;
-            case 'O':
-                modalContent.textContent = 'Player O have won!'
-                modal.style.display = 'block';
-                blureffect();
-                break;
-            case 'draw':
-                modalContent.textContent = "It's a draw!"
-                modal.style.display = 'block';
-                blureffect();
-                break;
-        }
+  };
+  //Displays the result on screen as a modal.
+  const haveWon = (res) => {
+    let modal = document.getElementById("myModal");
+    let modalContent = document.querySelector(".modal-content");
+    const gamewindow = document.querySelector(".gamewindow");
+    const blureffect = () => (gamewindow.style.filter = "blur(8px)");
+    window.onclick = (e) => {
+      if (e.target == modal) {
+        location.reload();
+      }
+    };
+    switch (res) {
+      case "X":
+        modalContent.textContent = "Player X have won!";
+        modal.style.display = "block";
+        blureffect();
+        break;
+      case "O":
+        modalContent.textContent = "Player O have won!";
+        modal.style.display = "block";
+        blureffect();
+        break;
+      case "draw":
+        modalContent.textContent = "It's a draw!";
+        modal.style.display = "block";
+        blureffect();
+        break;
     }
-    return {
-        dontAllowOccupiedSpace,
-        checkForWinOrDraw,
-    }
+  };
+  return {
+    dontAllowOccupiedSpace,
+    checkForWinOrDraw,
+  };
 })();
 
-
-const playGame = (()=> {
-    //displays next player on page
-    const whosNext = document.querySelector('.whosturn');
-    const nextPlayer = (cell) => {
-        if (cell.textContent ==='X'){
-           return whosNext.textContent = "Player O's turn";
-        }else if (cell.textContent === 'O'){
-           return whosNext.textContent = "Player X's turn";
-        }
-    };
-    //Handles the boardclicks and adds the markers.
-    document.querySelectorAll('.elem').forEach(cell => cell.addEventListener('click', ()=> {
-        cell.textContent = gameLogic.dontAllowOccupiedSpace(cell);
-        gameLogic.checkForWinOrDraw();
-        nextPlayer(cell);
-    }));
+const playGame = (() => {
+  //displays next player on page
+  const whosNext = document.querySelector(".whosturn");
+  const nextPlayer = (cell) => {
+    if (cell.textContent === "X") {
+      return (whosNext.textContent = "Player O's turn");
+    } else if (cell.textContent === "O") {
+      return (whosNext.textContent = "Player X's turn");
+    }
+  };
+  //Handles the boardclicks and adds the markers.
+  document.querySelectorAll(".elem").forEach((cell) =>
+    cell.addEventListener("click", () => {
+      cell.textContent = gameLogic.dontAllowOccupiedSpace(cell);
+      gameLogic.checkForWinOrDraw();
+      nextPlayer(cell);
+    })
+  );
 })();
